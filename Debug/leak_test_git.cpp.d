1a285f7d394bb573726e2e847332461a81c1a0e4
marcos - duque cesar <hepfive@gmail.com>
2016-10-11 12:51:28 +0900

    first

---
+++ Debug/leak_test_git.cpp.d
@@ -0,0 +1,15 @@
+C:\Users\himarc\AppData\Local\Temp\VMicroBuilds\leak_test_git\mega_atmega2560\leak_test_git.cpp.o: \
+ C:\Users\himarc\AppData\Local\Temp\VMicroBuilds\leak_test_git\mega_atmega2560\leak_test_git.cpp \
+ C:\Users\himarc\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.14\cores\arduino/arduino.h \
+ C:\Users\himarc\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.14\cores\arduino/binary.h \
+ C:\Users\himarc\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.14\cores\arduino/WCharacter.h \
+ C:\Users\himarc\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.14\cores\arduino/WString.h \
+ C:\Users\himarc\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.14\cores\arduino/HardwareSerial.h \
+ C:\Users\himarc\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.14\cores\arduino/Stream.h \
+ C:\Users\himarc\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.14\cores\arduino/Print.h \
+ C:\Users\himarc\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.14\cores\arduino/Printable.h \
+ C:\Users\himarc\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.14\cores\arduino/USBAPI.h \
+ C:\Users\himarc\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.14\cores\arduino/Arduino.h \
+ C:\Users\himarc\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.14\variants\mega/pins_arduino.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Extensions\4mbmmxhb.zzf\Micro\ Platforms\default\debuggers\VM_DBG/VM_DBG.h \
+ C:\Users\himarc\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.14\cores\arduino/Arduino.h
---
+++ leak_test_git.ino
@@ -0,0 +1,134 @@
+
+#include <Arduino.h>
+
+//Global Variables
+const byte BUTTON = 2; // our button pin
+const byte SENSOR = 3;
+
+const byte CYLINDERBIG = 4; // CYLINDERBIG (built-in on Uno)
+const byte CYLINDERSMALL = 5;
+byte comando = 0;
+unsigned long buttonPushedMillis; // when button was released
+unsigned long relayTurnedOnAt; // when CYLINDERBIG was turned on
+unsigned long turnOnDelay = 000; // wait to turn on CYLINDERBIG
+unsigned long turnOnDelay1 = 3000;
+
+unsigned long turnOffDelay = 9000; // turn off CYLINDERBIG after this
+unsigned long turnOffDelay1 = 7000;
+
+
+bool ledReady = false; // flag for when button is let go
+bool ledState = false; // for CYLINDERBIG is on or not.
+bool ledReady1 = false;
+bool ledState1 = false;
+
+
+
+void setup() {
+	pinMode ( SENSOR, INPUT );
+	pinMode ( BUTTON, INPUT_PULLUP );
+	pinMode ( CYLINDERBIG, OUTPUT );
+	pinMode ( CYLINDERSMALL, OUTPUT );
+	digitalWrite ( CYLINDERBIG, HIGH );
+	digitalWrite ( CYLINDERSMALL, HIGH );
+}
+
+void loop() {
+	// get the time at the start of this loop()
+	unsigned long currentMillis = millis();
+	int estado1 = digitalRead ( BUTTON );
+	int estado2 = digitalRead ( SENSOR );
+	
+	if ( estado1 == LOW && estado2 == LOW ) {
+		comando = 0;
+	}
+	
+	else
+		if ( estado1 == LOW && estado2 == HIGH ) {
+			comando = 1;
+		}
+		
+	switch ( comando ) {
+	case 0:
+		if ( digitalRead ( BUTTON ) == LOW ) {
+			// update the time when button was pushed
+			buttonPushedMillis = currentMillis;
+			ledReady = true;
+			ledReady1 = true;
+		}
+		
+		// make sure this code isn't checked until after button has been let go
+		if ( ledReady ) {
+			//this is typical millis code here:
+			if ( ( unsigned long ) ( currentMillis - buttonPushedMillis ) >= turnOnDelay ) {
+				// okay, enough time has passed since the button was let go.
+				digitalWrite ( CYLINDERBIG, LOW );
+				// setup our next "state"
+				ledState = true;
+				// save when the CYLINDERBIG turned on
+				relayTurnedOnAt = currentMillis;
+				// wait for next button press
+				ledReady = false;
+			}
+		}
+		
+		if ( ledReady1 ) {
+			//this is typical millis code here:
+			if ( ( unsigned long ) ( currentMillis - buttonPushedMillis ) >= turnOnDelay1 ) {
+				// okay, enough time has passed since the button was let go.
+				digitalWrite ( CYLINDERSMALL, LOW );
+				// setup our next "state"
+				ledState1 = true;
+				// save when the CYLINDERBIG turned on
+				relayTurnedOnAt = currentMillis;
+				// wait for next button press
+				ledReady1 = false;
+			}
+		}
+		
+		// see if we are watching for the time to turn off CYLINDERBIG
+		if ( ledState ) {
+			// okay, CYLINDERBIG on, check for now long
+			if ( ( unsigned long ) ( currentMillis - relayTurnedOnAt ) >= turnOffDelay ) {
+				ledState = false;
+				digitalWrite ( CYLINDERBIG, HIGH );
+			}
+		}
+		
+		if ( ledState1 ) {
+			// okay, CYLINDERBIG on, check for now long
+			if ( ( unsigned long ) ( currentMillis - relayTurnedOnAt ) >= turnOffDelay1 ) {
+				ledState1 = false;
+				digitalWrite ( CYLINDERSMALL, HIGH );
+			}
+		}
+		
+		break;
+		
+	case 1:
+		if ( digitalRead ( BUTTON ) == LOW ) {
+			// update the time when button was pushed
+			buttonPushedMillis = currentMillis;
+			ledReady = true;
+		}
+		
+		// make sure this code isn't checked until after button has been let go
+		if ( ledReady ) {
+			//this is typical millis code here:
+			if ( ( unsigned long ) ( currentMillis - buttonPushedMillis ) >= turnOnDelay ) {
+				// okay, enough time has passed since the button was let go.
+				digitalWrite ( CYLINDERBIG, HIGH );
+				// setup our next "state"
+				ledState = true;
+				// save when the CYLINDERBIG turned on
+				relayTurnedOnAt = currentMillis;
+				// wait for next button press
+				ledReady = false;
+			}
+		}
+		
+		break;
+	}
+}
+
+
