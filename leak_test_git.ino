dce442262cf512567289638c874b872d0044a7a9
marcos - duque cesar <hepfive@gmail.com>
2016-10-12 17:19:57 +0900

    third

--- leak_test_git.ino
+++ leak_test_git.ino
@@ -2,14 +2,15 @@
 #include <Arduino.h>
 
 //Global Variables
-const byte BUTTON = 2; // our button pin
+const byte BUTTON = 2 ; // our button pin
 const byte SENSOR = 3;
 const byte CYLINDERBIG = 4; // CYLINDERBIG (built-in on Uno)
 const byte CYLINDERSMALL = 5;
-const byte ledBig = 6;
+const byte LEDBIG = 6;
 const byte ledSmall = 7;
 
 byte comando = 0;
+
 unsigned long buttonPushedMillis; // when button was released
 unsigned long relayTurnedOnAt; // when CYLINDERBIG was turned on
 unsigned long turnOnDelay = 000; // wait to turn on CYLINDERBIG
@@ -18,7 +19,7 @@ unsigned long turnOnLedB = 000;
 unsigned long turnOnLedS = 3000;
 unsigned long turnOffDelay = 9000; // turn off CYLINDERBIG after this
 unsigned long turnOffDelay1 = 7000;
-unsigned long turnOffLedB = 9000;
+unsigned long turnOffLedB = 000;
 unsigned long turnOffLedS = 7000;
 
 bool ledReady = false; // flag for when button is let go
@@ -31,36 +32,36 @@ bool ledBigState = false;
 bool ledSmallState = false;
 
 void setup() {
-	pinMode ( SENSOR, INPUT );
-	pinMode ( BUTTON, INPUT_PULLUP );
-	pinMode ( CYLINDERBIG, OUTPUT );
-	pinMode ( CYLINDERSMALL, OUTPUT );
-	pinMode ( ledBig, OUTPUT );
-	pinMode ( ledSmall, OUTPUT );
-	digitalWrite ( CYLINDERBIG, HIGH );
-	digitalWrite ( CYLINDERSMALL, HIGH );
-	digitalWrite ( ledBig, HIGH );
-	digitalWrite ( ledSmall, HIGH );
+	pinMode(SENSOR, INPUT);
+	pinMode(BUTTON, INPUT_PULLUP);
+	pinMode(CYLINDERBIG, OUTPUT);
+	pinMode(CYLINDERSMALL, OUTPUT);
+	pinMode(LEDBIG, OUTPUT);
+	pinMode(ledSmall, OUTPUT);
+	digitalWrite(CYLINDERBIG, HIGH);
+	digitalWrite(CYLINDERSMALL, HIGH);
+	digitalWrite(LEDBIG, LOW);
+	digitalWrite(ledSmall, HIGH);
 }
 
 void loop() {
 	// get the time at the start of this loop()
 	unsigned long currentMillis = millis();
-	int estado1 = digitalRead ( BUTTON );
-	int estado2 = digitalRead ( SENSOR );
+	int estado1 = digitalRead(BUTTON);
+	int estado2 = digitalRead(SENSOR);
 
-	if ( estado1 == LOW && estado2 == LOW ) {
+	if (estado1 == LOW && estado2 == LOW) {
 		comando = 0;
 	}
 
 	else
-		if ( estado1 == LOW && estado2 == HIGH ) {
+		if (estado1 == LOW && estado2 == HIGH) {
 			comando = 1;
 		}
 
-	switch ( comando ) {
+	switch (comando) {
 	case 0:
-		if ( digitalRead ( BUTTON ) == LOW ) {
+		if (digitalRead(BUTTON) == LOW) {
 			// update the time when button was pushed
 			buttonPushedMillis = currentMillis;
 			ledReady = true;
@@ -70,11 +71,11 @@ void loop() {
 		}
 
 		// make sure this code isn't checked until after button has been let go
-		if ( ledReady ) {
+		if (ledReady) {
 			//this is typical millis code here:
-			if ( ( unsigned long ) ( currentMillis - buttonPushedMillis ) >= turnOnDelay ) {
+			if ((unsigned long)(currentMillis - buttonPushedMillis) >= turnOnDelay) {
 				// okay, enough time has passed since the button was let go.
-				digitalWrite ( CYLINDERBIG, LOW );
+				digitalWrite(CYLINDERBIG, LOW);
 				// setup our next "state"
 				ledState = true;
 				// save when the CYLINDERBIG turned on
@@ -84,11 +85,11 @@ void loop() {
 			}
 		}
 
-		if ( ledReady1 ) {
+		if (ledReady1) {
 			//this is typical millis code here:
-			if ( ( unsigned long ) ( currentMillis - buttonPushedMillis ) >= turnOnDelay1 ) {
+			if ((unsigned long)(currentMillis - buttonPushedMillis) >= turnOnDelay1) {
 				// okay, enough time has passed since the button was let go.
-				digitalWrite ( CYLINDERSMALL, LOW );
+				digitalWrite(CYLINDERSMALL, LOW);
 				// setup our next "state"
 				ledState1 = true;
 				// save when the CYLINDERBIG turned on
@@ -98,11 +99,11 @@ void loop() {
 			}
 		}
 
-		if ( ledBigReady ) {
+		if (ledBigReady) {
 			//this is typical millis code here:
-			if ( ( unsigned long ) ( currentMillis - buttonPushedMillis ) >= turnOnLedB ) {
+			if ((unsigned long)(currentMillis - buttonPushedMillis) >= turnOnLedB) {
 				// okay, enough time has passed since the button was let go.
-				digitalWrite ( ledBig, LOW );
+				digitalWrite(LEDBIG, LOW);
 				// setup our next "state"
 				ledBigState = true;
 				// save when the CYLINDERBIG turned on
@@ -112,46 +113,46 @@ void loop() {
 			}
 		}
 
-		if ( ledState ) {
+		if (ledState) {
 			// okay, CYLINDERBIG on, check for now long
-			if ( ( unsigned long ) ( currentMillis - relayTurnedOnAt ) >= turnOffDelay ) {
+			if ((unsigned long)(currentMillis - relayTurnedOnAt) >= turnOffDelay) {
 				ledState = false;
-				digitalWrite ( CYLINDERBIG, HIGH );
+				digitalWrite(CYLINDERBIG, HIGH);
 			}
 		}
 
-		if ( ledState1 ) {
+		if (ledState1) {
 			// okay, CYLINDERBIG on, check for now long
-			if ( ( unsigned long ) ( currentMillis - relayTurnedOnAt ) >= turnOffDelay1 ) {
+			if ((unsigned long)(currentMillis - relayTurnedOnAt) >= turnOffDelay1) {
 				ledState1 = false;
-				digitalWrite ( CYLINDERSMALL, HIGH );
+				digitalWrite(CYLINDERSMALL, HIGH);
 			}
 		}
 
 		// see if we are watching for the time to turn off CYLINDERBIG
-		if ( ledBigState ) {
+		if (ledBigState) {
 			// okay, CYLINDERBIG on, check for now long
-			if ( ( unsigned long ) ( currentMillis - relayTurnedOnAt ) >= turnOffLedB ) {
+			if ((unsigned long)(currentMillis - relayTurnedOnAt) >= turnOffLedB) {
 				ledReady = false;
-				digitalWrite ( ledBig, HIGH );
+				digitalWrite(LEDBIG, HIGH);
 			}
 		}
 
 		break;
 
 	case 1:
-		if ( digitalRead ( BUTTON ) == LOW ) {
+		if (digitalRead(BUTTON) == LOW) {
 			// update the time when button was pushed
 			buttonPushedMillis = currentMillis;
 			ledReady = true;
 		}
 
 		// make sure this code isn't checked until after button has been let go
-		if ( ledReady ) {
+		if (ledReady) {
 			//this is typical millis code here:
-			if ( ( unsigned long ) ( currentMillis - buttonPushedMillis ) >= turnOnDelay ) {
+			if ((unsigned long)(currentMillis - buttonPushedMillis) >= turnOnDelay) {
 				// okay, enough time has passed since the button was let go.
-				digitalWrite ( CYLINDERBIG, HIGH );
+				digitalWrite(CYLINDERBIG, HIGH);
 				// setup our next "state"
 				ledState = true;
 				// save when the CYLINDERBIG turned on
